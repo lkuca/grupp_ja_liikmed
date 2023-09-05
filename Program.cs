aa22202397fea3ad393f7fc67e4a554583def04b
opilane <opilane@HP-CZC2349HTQ>
2023-09-05 16:23:31 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using grupp_ja_liikmed;
+using System.Text.RegularExpressions;
+
+public class Program
+{
+    public static void Main()
+    {
+        
+
+        Group group = new Group(3);
+        
+        Console.WriteLine(group.AddMember("John"));  // True
+        Console.WriteLine(group.AddMember("John"));  // False
+        Console.WriteLine(group.AddMember("Mary"));  // True
+        Console.WriteLine(group.AddMember("Jane"));  // True
+        Console.WriteLine(group.AddMember("Joseph"));  // False
+        Console.WriteLine(string.Join(", ", group.Members));  // John, Mary, Jane
+        Console.WriteLine(group.GetMembersCount());  // 3
+
+        Group bookClub = new Group(4);
+        Console.WriteLine(bookClub.AddMember("Albert"));   // True
+        Console.WriteLine(bookClub.AddMember("Samantha"));  // True
+        Console.WriteLine(bookClub.AddMember("Robert"));   // True
+        Console.WriteLine(bookClub.AddMember("Roberta"));  // True
+        Console.WriteLine(string.Join(", ", bookClub.Members));  // Albert, Samantha, Robert, Roberta
+
+        Console.WriteLine(bookClub.HasMember("Albert"));  // True
+        Console.WriteLine(bookClub.HasMember("John"));  // False
+    }
+}
---
+++ liikmed.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace grupp_ja_liikmed
+{
+    public class liikmed
+    {
+
+        private readonly string Nimi;
+        private readonly int Aasta;
+        private readonly int Kuupäev;
+        private readonly int Kuu;
+
+
+        public liikmed(string nimi, int aasta, int kuupäev, int kuu)
+        {
+            this.Aasta = aasta;
+            this.Nimi = nimi;
+            this.Kuupäev = kuupäev;
+            this.Kuu = kuu;
+
+        }
+        public int aasta { get => Aasta; }
+        public int kuupäev { get => Kuupäev; }
+        public int kuu { get => Kuu; }
+        public string nimi { get => Nimi; }
+
+        public void randomyear()
+        {
+            Random r = new Random(); int aasta = r.Next(2000,2023);
+        }
+        public void randomkuupaev()
+        {
+            Random r = new Random(); int kuupäev = r.Next(1,31);
+        }
+        public void randomkuu()
+        {
+            Random r = new Random(); int kuu = r.Next(1,12);
+        }
+        public static char randomnimi(string s)
+        {
+
+        }
+        //    public List<string> Liik { get; } = new List<string>();
+        //    private readonly int daite;
+        //    public liikmed(int da_te)
+        //    {
+        //       daite  = da_te;
+        //    }
+
+
+        //    public bool AddDaite(string dat)
+        //    {
+        //        if (Liik.Contains(dat)) return false;
+        //        if (Liik.Count >= daite) return false;
+        //        Liik.Add(dat);
+        //        return true;
+        //    }
+
+        //    public int GetdaiteCount()
+        //    {
+        //        return Liik.Count;
+        //    }
+
+
+        //    public bool Hasdaite(string dat)
+        //    {
+        //        return Liik.Contains(dat);
+        //    }
+    }
+    
+}
